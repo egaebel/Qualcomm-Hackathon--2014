ffb105437c1d69c74998d18e62ad006d7398ecfb
unknown <mmastrov@MMASTROV1.na.qualcomm.com>
2014-06-21 06:40:30 -0700

    Finished ClientSocketHandler improvements.

--- app/src/qcom/hackathon/collab/download/ClientSocketHandler.java
+++ app/src/qcom/hackathon/collab/download/ClientSocketHandler.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.net.Socket;
+import java.util.List;
 
 /*
  * The non-group owners always act as clients in socket initialization.  
@@ -20,38 +21,26 @@ public class ClientSocketHandler extends Thread {
     private static final String TAG = "ClientSocketHandler";
     private Activity mAct= null;
     private InetAddress mAddress;
-    private int commType;
-    
-    private final int DISTRIBUTE = 4;
-    private final int DECISION = 1;
-    
-    private final int SEND = 0;
-    private final int RECEIVE = 1;
+    private Socket socket;
 
-    public ClientSocketHandler(Activity mAct, InetAddress groupOwnerAddress, int commType) {
+    public ClientSocketHandler(Activity mAct, InetAddress groupOwnerAddress) {
         this.mAct = mAct;
         this.mAddress = groupOwnerAddress;
-        this.commType = commType;
+    }
+    
+    public Socket getNonGOSocket() {
+    	return socket;
     }
 
     @Override
     public void run() {
-        Socket socket = new Socket();
+        socket = new Socket();
         try {
             socket.bind(null);
             socket.connect(new InetSocketAddress(mAddress.getHostAddress(),
                     WiFiDConnectionManager.SERVER_PORT), 5000);
             Log.d(TAG, "Launching the I/O handler");
-            if(commType == RECEIVE) {
-            	RecFileManager rec = new RecFileManager(socket, this.mAct);
-            	rec.receiveFile();
-            } else if(commType == SEND) {
-            	SendFileManager snd = new SendFileManager(socket, this.mAct);
-                snd.sendFile();
-            }
         
-//            chat = new ChatManager(socket, handler);
-//            new Thread(chat).start();
         } catch (IOException e) {
             e.printStackTrace();
             try {
