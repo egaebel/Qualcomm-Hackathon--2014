b2468ea0342b9aefaef624330295cd4cf409f0fa
J.J. Wang <canjian.myself@gmail.com>
2014-06-21 07:02:01 -0700

    add in basic function after connection set up

--- app/src/com/qualcomm/ui/MainActivity.java
+++ app/src/com/qualcomm/ui/MainActivity.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import qcom.hackathon.collab.download.ClientSocketHandler;
+import qcom.hackathon.collab.download.GroupOwnerSocketHandler;
 import qcom.hackathon.collab.download.R;
 import qcom.hackathon.collab.download.WiFiDConnectionManager;
 import qcom.hackathon.collab.download.WiFiP2pService;
@@ -39,12 +41,13 @@ import android.widget.Toast;
 @SuppressLint("NewApi")
 public class MainActivity extends Activity implements TabListener, ConnectionInfoListener {
 	public static RelativeLayout rl;
-	public static WiFiDConnectionManager wifi_ctrl = null; 
+	public  WiFiDConnectionManager wifi_ctrl = null; 
 	public static List<WiFiP2pService> peerList = null; 
 	static FragMent1 fram1;
 	static FragmentTransaction fragMentTra = null;
 	static FragMent2 fram2;
 	static FragMent3 fram3;
+	private String TAG = "mainactivity";
 	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -137,6 +140,7 @@ public class MainActivity extends Activity implements TabListener, ConnectionInf
 
 	
 	public void wel2peer(){
+		peerList = wifi_ctrl.getServiceList();
 		this.getActionBar().setSelectedNavigationItem(1);
 		for (WiFiP2pService eachDevice:peerList){
 			wifi_ctrl.connectP2p(eachDevice);
@@ -168,8 +172,35 @@ public class MainActivity extends Activity implements TabListener, ConnectionInf
 	 * connectioninfolistener, called when a connection is established
 	 */
 	@Override
-	public void onConnectionInfoAvailable(WifiP2pInfo info) {
-		Toast.makeText(getApplicationContext(), "connection established", Toast.LENGTH_LONG).show();;
+	public void onConnectionInfoAvailable(WifiP2pInfo p2pInfo) {
+        Thread handler = null;
+		Toast.makeText(getApplicationContext(), "connection established", Toast.LENGTH_LONG).show();;        
+        /*
+         * The group owner accepts connections using a server socket and then spawns a
+         * client socket for every client. This is handled by {@code
+         * GroupOwnerSocketHandler}
+         */
+        if (p2pInfo.isGroupOwner) {
+            Log.d(TAG, "Connected as group owner");
+            try {
+                handler = new GroupOwnerSocketHandler(this);
+                handler.start();
+            } catch (IOException e) {
+                Log.d(TAG,
+                        "Failed to create a server thread - " + e.getMessage());
+                return;
+            }
+        } else {
+            Log.d(TAG, "Connected as peer");
+            handler = new ClientSocketHandler(this,p2pInfo.groupOwnerAddress);
+            handler.start();
+        }
+        
+        try {
+        	handler.join();
+        } catch (InterruptedException e) {
+        	
+        }
 	}
 
 	
