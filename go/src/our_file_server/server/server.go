21e3ae5eb8951471e302a14f5da825f48c4c6775
Melissa Galonsky <mgalonsky@cs.hmc.edu>
2014-06-20 23:38:05 -0700

    Reverting to the original file server for testing.

--- go/src/our_file_server/server/server.go
+++ go/src/our_file_server/server/server.go
@@ -1,70 +1,11 @@
 package main
 
 import (
-	"os"
-	"io"
 	"log"
-	"net"
-	"fmt"
+	"net/http"
 )
 
-type Demand struct {
-	demand_size int32
-	groupID int32
-	group_size int32
-	chunk_size int32
-	filename string
-}
-
 func main() {
-	// Listen on TCP port 2000 on all interfaces.
-	l, err := net.Listen("tcp", ":80")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer l.Close()
-	for {
-		// Wait for a connection.
-		conn, err := l.Accept()
-
-		fmt.Println("Opening a new connection")
-
-		if err != nil {
-			log.Fatal(err)
-		}
-		// Handle the connection in a new goroutine.
-		// The loop then returns to accepting, so that
-		// multiple connections may be served concurrently.
-		go handle_connection(conn)
-	}
-}
-
-//Handles an incoming network connection
-//Currently echos data and writes to standard out
-//TO DO assign a chunk number and write back the chunk number,
-//      size of chunk, and the chunk
-func handle_connection(c net.Conn) {
-	//Ensures the connection
-	defer c.Close()
-
-	//Sets the maximum size of a buffer for a demand
-	const BUFFER_LENGTH = 1024
-	var buf [BUFFER_LENGTH]byte
-
-	n, err := c.Read(buf[0:])
-
-	_, err2 := c.Write(buf[0:n])
-
-	os.Stdout.Write(buf[0:n])
-}
-
-func CToGoString(c []byte) string {
-    n := -1
-    for i, b := range c {
-        if b == 0 {
-            break
-        }
-        n = i
-    }
-    return string(c[:n+1])
+	// Simple static webserver:
+	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("/usr/share/doc"))))
 }
