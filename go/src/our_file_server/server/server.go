f162b505225e2b6a9dde3ce4942269cc267c0fe3
Melissa Galonsky <mgalonsky@cs.hmc.edu>
2014-06-20 23:57:37 -0700

    Further server connection testing.

--- go/src/our_file_server/server/server.go
+++ go/src/our_file_server/server/server.go
@@ -1,32 +1,15 @@
 package main
 
 import (
-	"io"
-	"log"
-	"net"
+    "fmt"
+    "net/http"
 )
 
+func handler(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
 func main() {
-	// Listen on TCP port 2000 on all interfaces.
-	l, err := net.Listen("tcp", ":2000")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer l.Close()
-	for {
-		// Wait for a connection.
-		conn, err := l.Accept()
-		if err != nil {
-			log.Fatal(err)
-		}
-		// Handle the connection in a new goroutine.
-		// The loop then returns to accepting, so that
-		// multiple connections may be served concurrently.
-		go func(c net.Conn) {
-			// Echo all incoming data.
-			io.Copy(c, c)
-			// Shut down the connection.
-			c.Close()
-		}(conn)
-	}
+    http.HandleFunc("/", handler)
+    http.ListenAndServe(":8080", nil)
 }
