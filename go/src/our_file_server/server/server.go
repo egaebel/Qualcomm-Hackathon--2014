84ea5a0f03a76ac322acc0999718760fb16ec807
Melissa Galonsky <mgalonsky@cs.hmc.edu>
2014-06-21 00:30:12 -0700

    Reverting to our server code.

--- go/src/our_file_server/server/server.go
+++ go/src/our_file_server/server/server.go
@@ -1,15 +1,70 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
+	"os"
+	"io"
+	"log"
+	"net"
+	"fmt"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+type Demand struct {
+	demand_size int32
+	groupID int32
+	group_size int32
+	chunk_size int32
+	filename string
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":8080", nil)
+	// Listen on TCP port 2000 on all interfaces.
+	l, err := net.Listen("tcp", ":80")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer l.Close()
+	for {
+		// Wait for a connection.
+		conn, err := l.Accept()
+
+		fmt.Println("Opening a new connection")
+
+		if err != nil {
+			log.Fatal(err)
+		}
+		// Handle the connection in a new goroutine.
+		// The loop then returns to accepting, so that
+		// multiple connections may be served concurrently.
+		go handle_connection(conn)
+	}
+}
+
+//Handles an incoming network connection
+//Currently echos data and writes to standard out
+//TO DO assign a chunk number and write back the chunk number,
+//      size of chunk, and the chunk
+func handle_connection(c net.Conn) {
+	//Ensures the connection
+	defer c.Close()
+
+	//Sets the maximum size of a buffer for a demand
+	const BUFFER_LENGTH = 1024
+	var buf [BUFFER_LENGTH]byte
+
+	n, err := c.Read(buf[0:])
+
+	_, err2 := c.Write(buf[0:n])
+
+	os.Stdout.Write(buf[0:n])
+}
+
+func CToGoString(c []byte) string {
+    n := -1
+    for i, b := range c {
+        if b == 0 {
+            break
+        }
+        n = i
+    }
+    return string(c[:n+1])
 }
